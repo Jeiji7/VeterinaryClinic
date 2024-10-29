3f1644b8fca1e9e334bf8966e02a7f13a8b14a83
202114 <202114@1305-005>
2024-10-29 15:06:47 +0300

    Добавьте файлы проекта.

---
+++ VeterinaryClinic/Pages/EnterWorker.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VeterinaryClinic.Database;
+
+namespace VeterinaryClinic.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для EnterWorker.xaml
+    /// </summary>
+    public partial class EnterWorker : Page
+    {
+        public static List<Vet> vet { get; set; }
+        public EnterWorker()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Enter(object sender, RoutedEventArgs e)
+        {
+            string name = LastNameTB.Text.Trim();
+            string firstName = FirstNameTB.Text.Trim();
+
+            vet = new List<Vet>(App.db.Vet.ToList());
+            Vet currentVet = vet.FirstOrDefault(x => x.Vet_FirstName == name && x.Vet_LastName == firstName);
+            if (currentVet != null)
+            {
+                LastNameTB.Text = "";
+                FirstNameTB.Text = "";
+                NavigationService.Navigate(new Pages.RecordClient());
+            }
+            else
+            {
+                MessageBox.Show("Мы не нашли ваши данные в системе, попробуйте зайти снова");
+                LastNameTB.Text = "";
+                FirstNameTB.Text = "";
+            }
+        }
+    }
+}
